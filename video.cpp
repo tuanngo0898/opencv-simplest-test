ba10a48e00d3c6525b1c54bcde4acde71dd65a3c
tuannv <willingheart98@gmail.com>
2019-06-26 15:34:14 +0700

    Add video writer

--- video.cpp
+++ video.cpp
@@ -4,19 +4,26 @@ using namespace cv;
 
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
+    VideoCapture cap; // open the default camera
+    cap.open(0);
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-    Mat edges;
-    namedWindow("edges",1);
+    int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH); 
+    int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+    
+    VideoWriter video("outcpp.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height));
+    
+    Mat frame;
     for(;;)
     {
-        Mat frame;
         cap >> frame; // get a new frame from camera
+        video.write(frame);
         imshow("frame", frame);
         if(waitKey(30) >= 0) break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
+
+    cap.release();
+    video.release();
     return 0;
 }
